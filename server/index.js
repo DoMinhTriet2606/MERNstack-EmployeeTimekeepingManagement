202040347a971cc5ebeb49f664375867c11a9d3a
Minh Triet <nightcorekz69@gmail.com>
2024-05-10 18:27:58 +0700

    build: deploy server to Heroku

--- server/index.js
+++ server/index.js
@@ -27,14 +27,14 @@ const connectDB = async () => {
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
-app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userInfoRouter, userShiftRouter, salaryRouter);
 app.use("/api/admin", adminRouter);
+
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`listen on port ${PORT}`);
