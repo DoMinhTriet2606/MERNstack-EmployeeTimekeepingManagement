54490a152e0c9b8474b42e8d45aa38c91cb33320
Minh Triet <nightcorekz69@gmail.com>
2024-05-10 18:55:05 +0700

    build: deploy server to Heroku

--- server/index.js
+++ server/index.js
@@ -36,6 +36,12 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.all("/", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/user", userInfoRouter, userShiftRouter, salaryRouter);
 app.use("/api/admin", adminRouter);
